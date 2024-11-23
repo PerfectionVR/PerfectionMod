bf597122f01a010f334732260b5d6989f87dc260
PerfectionVR <perfectioncsgo@gmail.com>
2024-11-23 16:00:32 +0100

    try optimize distributions

--- media/lua/server/Items/Distributions_Bokuto.lua
+++ media/lua/server/Items/Distributions_Bokuto.lua
@@ -1,12 +1,40 @@
-if getSandboxOptions():getOptionByName("EnableBokuto"):getValue() then
-    -- Fetch the drop chance as a percentage and divide by 100 to convert to a decimal
-    local bokutoDropChance = getSandboxOptions():getOptionByName("BokutoModDropChance"):getValue() / 100
+if isServer() then
+    -- Function to configure loot distributions based on sandbox options
+    local function setupLootDistributions()
+        -- Fetch sandbox options
+        local enableBokuto = getSandboxOptions():getOptionByName("EnableBokuto"):getValue()
+        local bokutoDropChance = getSandboxOptions():getOptionByName("BokutoDropChance"):getValue() / 100
 
-    -- Add the Bokuto book to BookstoreBooks distribution
-    table.insert(ProceduralDistributions.list["BookstoreBooks"].items, "BokutoMod.BokutoCraftingBook")
-    table.insert(ProceduralDistributions.list["BookstoreBooks"].items, bokutoDropChance)
+        -- Get the distribution list for BookstoreBooks
+        local distributionList = ProceduralDistributions.list["BookstoreBooks"]
 
-    -- Optionally add it to other distributions, e.g., LibraryBooks
-    table.insert(ProceduralDistributions.list["LibraryBooks"].items, "BokutoMod.BokutoCraftingBook")
-    table.insert(ProceduralDistributions.list["LibraryBooks"].items, bokutoDropChance)
+        -- Function to remove Bokuto items from the distribution list
+        local function removeBokutoItems()
+            local updatedItems = {}
+            for i = 1, #distributionList.items, 2 do
+                local item = distributionList.items[i]
+                local chance = distributionList.items[i + 1]
+
+                -- Keep all items except the Bokuto crafting book
+                if item ~= "BokutoMod.BokutoCraftingBook" then
+                    table.insert(updatedItems, item)
+                    table.insert(updatedItems, chance)
+                end
+            end
+            distributionList.items = updatedItems
+        end
+
+        -- If Bokuto is enabled, add it to distributions; otherwise, remove it
+        if enableBokuto then
+            table.insert(distributionList.items, "BokutoMod.BokutoCraftingBook")
+            table.insert(distributionList.items, bokutoDropChance)
+            print("Bokuto crafting book added to loot distributions.")
+        else
+            removeBokutoItems()
+            print("Bokuto crafting book removed from loot distributions.")
+        end
+    end
+
+    -- Hook into the OnInitWorld event (called when the server initializes the world)
+    Events.OnInitWorld.Add(setupLootDistributions)
 end
---
+++ media/lua/server/PerfectionModServer.lua
@@ -0,0 +1,4 @@
+if isServer() then
+    -- Notify that the mod has been successfully loaded
+    print("PerfectionMod successfully loaded on the server.")
+end
